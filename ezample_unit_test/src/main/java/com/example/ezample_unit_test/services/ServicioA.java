1878fcacfa3a4f88636f7a9b5ee8f13ed1c0ba41
euclidesseg <euder0206@gmail.com>
2023-09-24 23:35:00 -0500

    Unit test servicio

--- ezample_unit_test/src/main/java/com/example/ezample_unit_test/services/ServicioA.java
+++
@@ -1,5 +0,0 @@
-package com.example.ezample_unit_test.services;
-
-public interface ServicioA {
-    public abstract int sumar(int a, int b);
-}
--- ezample_unit_test/src/main/java/com/example/ezample_unit_test/services/ServicioB.java
+++
@@ -1,9 +0,0 @@
-package com.example.ezample_unit_test.services;
-public interface ServicioB{
-
-    public ServicioA getServicioA();
-
-    public void setServicioA(ServicioA servicioA);
-    public int multiplicarSumar(int a, int b, int multiplicador);
-    public int multiplicar(int a, int b);
-}
